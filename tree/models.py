b8078d9fcb8674302ac0f0f043cb8a94b88a3fe5
Shilin Georgy <shilingeorgiy@yandex.ru>
2020-02-02 16:02:01 +0300

    Preparing

--- tree/models.py
+++ tree/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
+
 # Create your models here.
 class Tree(models.Model):
-   parent = models.ForeignKey('self', models.CASCADE, null=True, verbose_name='Родитель')
-   name = models.CharField(max_length=255, verbose_name='Название')
+    parent = models.ForeignKey('self', models.CASCADE, null=True, verbose_name='Родитель')
+    name = models.CharField(max_length=255, verbose_name='Название')
